155a26e4b618edab475f86b5bae80123620d1425
Guilherme <guilhermemagno09@gmail.com>
2023-01-24 22:56:03 -0300

    Learning About data and components

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Learning from "./components/Learning.vue";
+</script>
+
+<template>
+  <h1>Learning {{ skill }}</h1>
+  <Learning />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      skill: "Vue",
+    };
+  },
+};
+</script>
---
+++ src/components/Learning.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>I'm {{ nome }} and I'm learning {{ skills }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: "Guilherme",
+      skills: "React and Vue",
+    };
+  },
+};
+</script>
