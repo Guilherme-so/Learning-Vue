6725b1825ef31ab34ffd98a6e245d31e9205aa86
Guilherme <guilhermemagno09@gmail.com>
2023-01-24 23:12:21 -0300

    Learning about lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import Learning from "./components/Learning.vue";
+import LifeCycle from "./components/LifeCycle.vue";
 </script>
 
 <template>
   <h1>Learning {{ skill }}</h1>
   <Learning />
+
+  <LifeCycle/>
 </template>
 
 <script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Let's undestand lifecycle of vue</h1>
+  <p>Here's my data: {{ data }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: "First data",
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.data = "Second Data";
+    }, 1000);
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.data = "Third Data";
+    }, 2000);
+  },
+};
+</script>
