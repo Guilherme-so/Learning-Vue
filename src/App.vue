a4254ccf9aa096a903f9516e26da85f0a54fa7af
Guilherme <guilhermemagno09@gmail.com>
2023-01-27 14:31:20 -0300

    Learning about Dinamic atribucts

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
 import Learning from "./components/Learning.vue";
 import LifeCycle from "./components/LifeCycle.vue";
 import Diretives from "./components/Diretives.vue";
+import Dinamicos from "./components/Dinamicos.vue"
 </script>
 
 <template>
   <h1>Learning {{ skill }}</h1>
   <!-- <Learning /> -->
   <!-- <LifeCycle /> -->
+  <!-- <Diretives /> -->
 
-  <Diretives />
+  <Dinamicos />
 </template>
 
 <script>
---
+++ src/components/Dinamicos.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Dados dinamicos</h1>
+  <p>
+    Segue o link para o site
+    <a v-bind:href="link_externo" target="_blank"> Clickando aqui</a>
+  </p>
+  <img :src="avatar" :alt="avatar_alt" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      link_externo: "https://google.com",
+      avatar: "https://github.com/Guilherme013S.png",
+      avatar_alt: "github avatar",
+    };
+  },
+};
+</script>
